82c1d1868e086d9fc51512c1c96e8a2750060afb
Anthony Bloomer <ant0@protonmail.ch>
2019-03-07 20:22:32 +0000

    First commit

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,25 @@
+import newrelic.agent
+
+newrelic.agent.initialize('newrelic.ini')
+newrelic.agent.register_application(timeout=10.0)
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    @newrelic.agent.background_task()
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
